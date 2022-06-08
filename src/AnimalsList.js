ffcfb4e121f422d67821616d77a939e5cc03f41f
David <Rigby335@hotmail.com>
2022-06-08 16:51:52 -0700

    removed console.log

--- src/AnimalsList.js
+++ src/AnimalsList.js
@@ -1,7 +1,6 @@
 import AnimalItem from './AnimalItem';
 
 export default function AnimalsList({ animals }) {
-  console.log(animals);
   return <div>
     {
       animals.map((animal, i) => <AnimalItem {...animal} key={animal + i} />)
--- src/DiscsList.js
+++ src/DiscsList.js
@@ -1,7 +1,6 @@
 import DiscItem from './DiscItem';
 
 export default function DiscsList({ discs }) {
-  console.log(discs);
   return <div>
     {
       discs.map((disc, i) => <DiscItem disc={disc} key={disc + i} />)
